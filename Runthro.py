cea92d3db46ced95196d06e0762f082609d484aa
Nicholas9090 <lazidisnikos@gmail.com>
2020-02-13 09:13:41 +0000

    Add files via upload

---
+++ Runthro.py
@@ -0,0 +1,42 @@
+#class Character:
+
+
+
+Name_A = Nicholas
+Surname_A = Lazidis
+year_of_birth_A = 1990
+
+Name_B = Egle
+Surname_B = Vysniauskaite
+year_of_birth_B = 1992
+
+Name_C = Michael
+Surname_C = Tsardakas
+year_of_birth_C = 1986
+
+
+input('Who are you?')
+user_input = input("What year were you born?")
+
+
+if int(user_input) == year_of_birth_A:
+    print("You're a youngster")
+else:   int(user_input) == year_of_birth_B
+    print("You're a baby")
+elif:   int(user_input) == year_of_birth_C
+    print("You're an old bum")
+
+
+#x = 10
+#y = 6
+
+#if x >= y:
+#    print('Hooray')
+#else:
+#    print('Gutted')
+
+#question = input('What is the ideal number?')
+#if int(question) > y:
+#    print('That is it')
+#else:
+#    print('Fuck off')
